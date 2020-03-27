df382acc9004d3b5f9b50f513c2dc66f84242808
11461648742 <yara.araujo@unirio.br>
2020-03-27 10:40:40 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
+//JSX - (js+xml) quando html está dentro do js
+//Componente: função do react que retorna html (podendo ter js, css)
+//Propriedade: similar aos atributos do html, porém mais poderosos. O 'title' do header é um exemplo.
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Omnistack!
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes/>
   );
 }
 
